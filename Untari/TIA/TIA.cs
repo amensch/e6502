f706eb03eb0fdb0d4fa77dfd6701a1e9e0090740
Mensch, Adam <adam.mensch@dematic.com>
2016-11-29 16:37:27 -0600

    Continue TIA coding

--- Untari/TIA/TIA.cs
+++ Untari/TIA/TIA.cs
@@ -3,21 +3,51 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using e6502CPU;
 
 namespace Untari.TIA
 {
     public class TIA
     {
+        /*
+         * TIA Registers: $0000-007F
+         * 
+         * General TIA Timing:
+         * 1 CPU clock is 3 TIA clocks
+         * Horizontal Timing:  68 TIA clocks hBlank, 160 TIA clocks drawing
+         * Total of 228 horizontal scan lines
+         * Vertical Timing: 3 lines vSync, 37 lines vBlank, 192 lines drawing, 30 lines overscan
+         * Total of 262 vertical scan lines
+         * 
+         * WSYNC regsiter is used to halt the CPU and resyncronize at the end of a line
+         * 
+         * Vertical Timing:
+         *  When 262 lines have been scanned the TV must be signaled to blank the beam and return to the top.
+         *  The TIA must transmit vertical sync for 3 scanlines by writing a "1" in D1 of VSYNC register,
+         *  count 3 lines, then write a "0" in D1 of VSYNC.
+         *  
+         *  To turn off the beam a "1" in D1 of VBLANK is set, then 37 lines are counted and then 
+         *  a "0" is written in D1 of VBLANK.
+         *
+         */
+
+
         private const int HBLANK = 68;
         private const int VDRAWMIN = 41;
         private const int VDRAWMAX = 232;
 
+        private const int HMAX = 228;
+        private const int VMAX = 262;
+
+        private e6502 cpu;
 
         private int h_line;
         private int v_line;
 
-        public TIA()
+        public TIA(e6502 _cpu)
         {
+            cpu = _cpu;
+
             // initial state?
             h_line = 0;
             v_line = 0;
@@ -27,7 +57,36 @@ namespace Untari.TIA
         {
             // 3 TIA clocks for every 6502 clock.
             // Update state machine and make any other necessary updates.
-            int new_hline = h_
+            int ticks = cpuclocks * 3;
+
+            for( int ii = 1; ii <= ticks; ii++ )
+            {
+                h_line++;
+                if( h_line > HMAX)
+                {
+                    h_line = 1;
+                    v_line++;
+                    if(v_line > VMAX)
+                    {
+                        v_line = 1;
+                    }
+                }
+                Draw();
+            }
+
+        }
+
+        private void Draw()
+        {
+            // check for transition of VSYNC
+            // check for transition of VBLANK
+            // check for use of WSYNC
+
+            // this is the actual drawing area of the screen
+            if( ( h_line > HBLANK ) && ( v_line > VDRAWMIN ) && ( v_line < VDRAWMAX ) )
+            {
+
+            }
         }
 
 
---
+++ Untari/TIA/TIAAddress.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Untari.TIA
+{
+    public class TIAAddress
+    {
+        // Write Addresses
+        public const int VSYNC = 0x00;      // vertical sync set/clear
+        public const int VBLANK = 0x01;     // vertical blank set/clear
+        public const int WSYNC = 0x02;      // wait for leading edge of horizontal blank
+        public const int RSYNC = 0x03;      // reset horizontal sync counter
+        public const int NUSIZ0 = 0x04;     // number-size player-missle 0
+        public const int NUSIZ1 = 0x05;     // number-size player-missle 1
+        public const int COLUP0 = 0x06;     // color-lum player 0
+        public const int COLUP1 = 0x07;     // color-lum player 1
+        public const int COLUPF = 0x08;     // color-lum playfield
+        public const int COLUBK = 0x09;     // color-lum background
+        public const int CTRLPF = 0x0a;     // control playfield ball size & collisions
+        public const int REFP0 = 0x0b;      // reflect player 0
+        public const int REFP1 = 0x0c;      // reflect player 1
+        public const int PF0 = 0x0d;        // playfield register byte 0
+        public const int PF1 = 0x0e;        // playfield register byte 1
+        public const int PF2 = 0x0f;        // playfield register byte 2
+        public const int RESP0 = 0x10;      // reset player 0
+        public const int RESP1 = 0x11;      // reset player 1
+        public const int RESM0 = 0x12;      // reset missile 0
+        public const int RESM1 = 0x13;      // reset missile 1
+        public const int RESBL = 0x14;      // reset ball
+        public const int AUDC0 = 0x15;      // audio control 0
+        public const int AUDC1 = 0x16;      // audio control 1
+        public const int AUDF0 = 0x17;      // audio frequency 0
+        public const int AUDF1 = 0x18;      // audio frequency 1
+        public const int AUDV0 = 0x19;      // audio volume 0
+        public const int AUDV1 = 0x1a;      // audio volume 1
+        public const int GRP0 = 0x1b;       // graphics player 0
+        public const int GRP1 = 0x1c;       // graphics player 1
+        public const int ENAM0 = 0x1d;      // graphics enable missle 0
+        public const int ENAM1 = 0x1e;      // graphics enable missle 1
+        public const int ENABL = 0x1f;      // graphics enable ball
+        public const int HMP0 = 0x20;       // horizontal motion player 0
+        public const int HMP1 = 0x21;       // horizontal motion player 1
+        public const int HMM0 = 0x22;       // horizontal motion missile 0
+        public const int HMM1 = 0x23;       // horizontal motion missile 1
+        public const int HMBL = 0x24;       // horizontal motion ball
+        public const int VDELP0 = 0x25;     // vertical delay player 0
+        public const int VDELP1 = 0x26;     // vertical delay player 1
+        public const int VDELBL = 0x27;     // vertical delay ball
+        public const int RESMP0 = 0x28;     // reset missle 0 to player 0
+        public const int RESMP1 = 0x29;     // reset missle 1 to player 1
+        public const int HMOVE = 0x2a;      // apply horizontal motion
+        public const int HMCLR = 0x2b;      // clear horizontal motion registers
+        public const int CXCLR = 0x2c;      // clear collision latches
+
+        // Read Addresses
+        public const int CXM0P = 0x00;
+        public const int CXM1P = 0x01;
+        public const int CXP0FB = 0x02;
+        public const int CXP1FB = 0x03;
+        public const int CXM0FB = 0x04;
+        public const int CXM1FB = 0x05;
+        public const int CXBLPF = 0x06;
+        public const int CXPPMM = 0x07;
+        public const int INPT0 = 0x08;
+        public const int INPT1 = 0x09;
+        public const int INPT2 = 0x0a;
+        public const int INPT3 = 0x0b;
+        public const int INPT4 = 0x0c;
+        public const int INPT5 = 0x0d;
+    }
+}
