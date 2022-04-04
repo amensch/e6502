500dcfbec04e1b7d5b0b1793875c82b83f983c96
Adam Mensch <adam@menschwi.com>
2022-04-03 20:13:53 -0500

    Push to test CI action

--- e6502/Utility/CPUMath.cs
+++ e6502/Utility/CPUMath.cs
@@ -5,7 +5,7 @@
         public static byte BCDToHex(int result)
         {
             if (result > 0xff)
-                throw new InvalidOperationException("Invalid BCD to hex number: " + result.ToString());
+                throw new InvalidOperationException($"Invalid BCD to hex number: {result}");
 
             if (result <= 9)
                 return (byte)result;
@@ -17,9 +17,9 @@
         {
             // validate input is valid packed BCD 
             if (oper > 0x99)
-                throw new InvalidOperationException("Invalid BCD number: " + oper.ToString("X2"));
+                throw new InvalidOperationException($"Invalid BCD number: {oper:X2}");
             if ((oper & 0x0f) > 0x09)
-                throw new InvalidOperationException("Invalid BCD number: " + oper.ToString("X2"));
+                throw new InvalidOperationException($"Invalid BCD number: {oper:X2}");
 
             return ((oper >> 4) * 10) + (oper & 0x0f);
         }
